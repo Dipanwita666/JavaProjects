55f26ad405ac5f1451df45cda3450b728945c1f7
mlove <mlove@LAPTOP-D0DEH5BL>
2022-03-22 16:41:31 +0530

    Adding files

---
+++ src/com/assignment/Box.java
@@ -0,0 +1,17 @@
+package com.assignment;
+
+public class Box {
+    double h,w,d;
+
+     Box(double width,double height,double depth)
+    {
+        h=height;
+        w=width;
+        d=depth;
+    }
+    double volume()
+    { double v;
+    v=h*w*d;
+    return v;
+    }
+}
---
+++ src/com/assignment/Main.java
@@ -0,0 +1,8 @@
+package com.assignment;
+
+public static void main(String[] args) {
+    
+    Box bc = new Box(8.5,80.3,9.6);
+    System.out.println(bc.volume());
+
+    }
